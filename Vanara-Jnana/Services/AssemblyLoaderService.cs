74f7082b9308c74036c04c56a0ceef24c360d989
Thorsten Jung <tajbender@users.noreply.github.com>
2026-04-27 12:10:29 +0200

    Add `NuGetUtils` and `AssemblyLoaderService`

---
+++ Vanara-Jnana/Services/AssemblyLoaderService.cs
@@ -0,0 +1,32 @@
+﻿using ClassicSamplesBrowser.Vanara.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassicSamplesBrowser.Vanara.NuGet;
+
+namespace ClassicSamplesBrowser.Services;
+
+internal class AssemblyLoaderService
+{
+    public async Task<IElementInfo?> LoadVanaraAssemblyTreeAsync()
+    {
+        // Get Vanara packages from NuGet
+//        var packages = await NuGetUtils.GetVanaraPackagesAsync();
+//
+//        // Select the latest version (simple for now)
+//        var latest = packages.OrderByDescending(p => p.Version).FirstOrDefault();
+//        if (latest == null)
+//            return null;
+//
+//        // Download package and extract DLLs
+//        var dllPaths = await NuGetUtils.DownloadAndExtractAssembliesAsync(latest);
+//
+//        // Create reflection tree
+//        var root = AssemblyElements.CreateFromAssemblies(dllPaths);
+//
+//        return root;
+        return null;
+    }
+}
