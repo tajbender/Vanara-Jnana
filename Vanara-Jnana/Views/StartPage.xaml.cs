d8676cd7f3d9e79fb00f894a1f13874228a2cff2
Thorsten Jung <tajbender@users.noreply.github.com>
2026-05-02 17:58:34 +0200

    Add `SidebarControl`. Add Sidebar to `StartPage`

--- Vanara-Jnana/Views/StartPage.xaml.cs
+++ Vanara-Jnana/Views/StartPage.xaml.cs
@@ -1,5 +1,11 @@
+using ClassicSamplesBrowser.Vanara.NuGet;
+using ClassicSamplesBrowser.Vanara.Reflection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using NuGet.Common;
+using NuGet.Protocol.Core.Types;
+using System.Collections.ObjectModel;
+using NuGet.Packaging;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -11,24 +17,29 @@ namespace ClassicSamplesBrowser.Views;
 /// </summary>
 public sealed partial class StartPage : Page
 {
+    const string framework = "net8.0";
+    private const string Prefix = "Vanara";
+    readonly List<IPackageSearchMetadata> packages = [];
+    static readonly ILogger logger = NullLogger.Instance; // TODO: Replace with actual logger if needed
+    static readonly CancellationToken cancellationToken = CancellationToken.None;
+
+    //internal ObservableCollection<IElementInfo> RootItems { get; } = [];
+    public ObservableCollection<IPackageSearchMetadata> RootItems { get; } = [];
+
+
+
     public StartPage()
     {
         InitializeComponent();
-        LoadVersions();
+        this.DataContext = this;
+
+        this.Loading += StartPage_Loading;
     }
 
-    private async void LoadVersions()
+    private void StartPage_Loading(FrameworkElement sender, object args)
     {
-        // TODO: Replace with David's NuGet API helper
-        var versions = new List<string>
-            {
-                "Latest Version: 5.0.4",
-                "5.0.4",
-                "5.0.3",
-                "5.0.2",
-                "5.0.1",
-                "5.0.0",
-            };
+
+
     }
 
     private void OpenExplorer_Click(object sender, RoutedEventArgs e)
@@ -43,6 +54,13 @@ public sealed partial class StartPage : Page
 
     private void LoadAssemblies_Click(object sender, RoutedEventArgs e)
     {
-        // TODO: Hook into David's Assembly Loader
+        Task.Factory.StartNew(async () =>
+        {
+            await foreach (var package in NuGetUtils.LoadNuGetPackageListAsync(Prefix, logger, cancellationToken))
+                if (package.Identity.Id.StartsWith(Prefix + '.', StringComparison.OrdinalIgnoreCase))
+                    packages.Add(package);
+        }, cancellationToken);
+
+        
     }
 }
