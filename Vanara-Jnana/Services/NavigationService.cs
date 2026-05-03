cab3bd643d0b1b0eb4d7233b9a8d13f8ae6c7d2c
Thorsten Jung <tajbender@users.noreply.github.com>
2026-05-03 14:22:39 +0200

    Add `NavigationService` stub

---
+++ Vanara-Jnana/Services/NavigationService.cs
@@ -0,0 +1,34 @@
+﻿using ClassicSamplesBrowser.Views;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassicSamplesBrowser.Services;
+
+public static class NavigationService
+{
+    private static Frame _frame;
+
+    public static void Init(Frame frame)
+    {
+        _frame = frame;
+    }
+
+    public static void Navigate(string tag)
+    {
+        Type page = tag switch
+        {
+            "Start" => typeof(StartPage),
+            "API" => typeof(ApiExplorerPage),
+            "Samples" => typeof(SamplesPage),
+            _ => null
+        };
+
+        // TODO: Fix this. It should be possible to navigate to a page that is not in the switch statement, but it should be in the same assembly as the other pages.
+        if (page != null)
+            _frame.Navigate(page);
+    }
+}
--- Vanara-Jnana/Vanara/Controls/SidebarControl.xaml.cs
+++ Vanara-Jnana/Vanara/Controls/SidebarControl.xaml.cs
@@ -1,4 +1,7 @@
+using ClassicSamplesBrowser.Services;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -10,5 +13,15 @@ public sealed partial class SidebarControl : UserControl
     public SidebarControl()
     {
         InitializeComponent();
+        Loaded += SidebarControl_Loaded;
+    }
+
+    private void SidebarControl_Loaded(object sender, RoutedEventArgs e)
+    {
+        foreach (var child in SidebarPanel.Children)
+        {
+            if (child is ToggleButton btn)
+                btn.Click += (s, e2) => NavigationService.Navigate(btn.Tag.ToString());
+        }
     }
 }
