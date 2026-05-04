707d7c562e05fa6b7c74939fdd43596700f72623
Thorsten Jung <tajbender@users.noreply.github.com>
2026-05-04 21:17:45 +0200

    Cleanup

--- Vanara-Jnana/MainWindow.xaml.cs
+++ Vanara-Jnana/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Input;
 using System.Runtime.InteropServices;
+using Vanara.PInvoke;
 using WinRT;
 
 namespace ClassicSamplesBrowser;
@@ -36,28 +37,21 @@ public sealed partial class MainWindow : Window
 //        ShowSystemMenu();
 //    }
 //
-    public void ShowSystemMenu() => ShowSystemMenu(target: this, uFlags: 0x0000, bRevert: false);
+    public void ShowSystemMenu() => ShowSystemMenu(targetObject: this, uFlags: 0x0000, bRevert: false);
 
-    public static void ShowSystemMenu(object target, uint uFlags = 0x0000, bool bRevert = false)
+    public static void ShowSystemMenu(object targetObject, uint uFlags = 0x0000, bool bRevert = false)
     {
-        // Get the window handle (HWND) of the current window using Vanara interop
         // TODO: Add support for right-clicking the title bar to show the system menu, and for showing the system menu at the cursor position instead of the top-left corner of the window
         // TODO: Handle exceptions that may occur when calling the Win32 API functions, such as if the window handle is invalid or if the system menu cannot be retrieved or displayed
-        var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(target);
-        var menu = GetSystemMenu(hwnd, bRevert);
+        var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(targetObject);
+
+        var menu = User32.GetSystemMenu(hwnd, bRevert);
 
         var point = new Windows.Graphics.PointInt32(0, 0);
-        //PInvoke.GetCursorPos(out point);
 
-        TrackPopupMenu(menu, uFlags, point.X, point.Y, 0, hwnd, IntPtr.Zero);
-        return;
+        User32.TrackPopupMenuFlags tpopMenuFlags = User32.TrackPopupMenuFlags.TPM_LEFTBUTTON;
 
-        [DllImport("user32.dll")]
-        static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
-
-        [DllImport("user32.dll")]
-        static extern bool TrackPopupMenu(IntPtr hMenu, uint uFlags, int x, int y,
-            int nReserved, IntPtr hWnd, IntPtr prcRect);
+        User32.TrackPopupMenu(menu, tpopMenuFlags, point.X, point.Y, 0, hwnd);
     }
 
     private void Window_Activated(object sender, WindowActivatedEventArgs args)
