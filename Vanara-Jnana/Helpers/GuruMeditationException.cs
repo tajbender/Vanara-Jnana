89b2f35ee897bcbe8cd5f68967b0a534c5a39459
Thorsten Jung <tajbender@users.noreply.github.com>
2026-06-01 21:23:12 +0200

    Added `GuruMeditationException`

---
+++ Vanara-Jnana/Helpers/GuruMeditationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jnana.Helpers;
+
+class GuruMeditationException
+{
+}
