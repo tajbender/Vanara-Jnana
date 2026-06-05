acaac4b6fac72b3095d1e7d0085c149691e2d3ff
Thorsten Jung <tajbender@users.noreply.github.com>
2026-06-05 18:56:11 +0200

    Rename and Cleanup `DisassemblerPage`

--- Vanara-Jnana/Views/DisassemblerPage.xaml.cs
+++ Vanara-Jnana/Views/DisassemblerPage.xaml.cs
@@ -13,19 +13,12 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace Jnana.Views;
 
-namespace ClassicSamplesBrowser.Views
+public sealed partial class DisassemblerPage : Page
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class AssembliesPage : Page
+    public DisassemblerPage()
     {
-        public AssembliesPage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
