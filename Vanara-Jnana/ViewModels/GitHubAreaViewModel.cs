07eebeb54b80cae82e429ccf7a4faa5e7dc04aa1
Thorsten Jung <tajbender@users.noreply.github.com>
2026-06-06 14:53:27 +0200

    Remove explicit `WebView2` package references and refactor

--- Vanara-Jnana/ViewModels/GitHubAreaViewModel.cs
+++ Vanara-Jnana/ViewModels/GitHubAreaViewModel.cs
@@ -1,11 +1,14 @@
-﻿using ClassicSamplesBrowser.Vanara.Reflection;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Jnana.Vanara.NuGet;
 using System.Collections.ObjectModel;
+using System.Security.Policy;
 
 namespace Jnana.ViewModels;
 
 public partial class GitHubAreaViewModel : ObservableObject
 {
+    private Url GitHubRepoUrl { get; } = new("https://github.com/vanara/Vanara");
+    private Url GitHubIssuesUrl { get; } = new("https://github.com/vanara/Vanara/issues");
+    private Url GitHubPullRequestsUrl { get; } = new("https://github.com/vanara/Vanara/pulls");
 }
--- Vanara-Jnana/Views/VoidPage.xaml.cs
+++ Vanara-Jnana/Views/VoidPage.xaml.cs
@@ -8,6 +8,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -24,27 +25,42 @@ public sealed partial class VoidPage : Page
         InitializeComponent();
         // TODO: ViewModel = new VoidPageViewModel();
 
-//        GitHubPreview.CoreWebView2Initialized += (_, __) =>
-//        {
-//            // TODO: Is this call ever reached?
-//            var wv = GitHubPreview.CoreWebView2;
-//
-//            // Disable context menus, dev tools, zoom, and status bar
-//            wv.Settings.AreDefaultContextMenusEnabled = false;
-//            wv.Settings.AreDevToolsEnabled = false;
-//            wv.Settings.IsZoomControlEnabled = false;
-//            wv.Settings.IsStatusBarEnabled = false;
-//
-//            // Disable built-in error page to prevent it from showing when loading fails
-//            //wv.Settings.IsBuiltInErrorPageEnabled = false;
-//
-//            // Inject CSS to hide Scrollbars and prevent scrolling (optional, can be adjusted as needed)
-//            //wv.AddScriptToExecuteOnDocumentCreatedAsync(@"
-//            //const style = document.createElement('style');
-//            //style.innerHTML = 
-//            //    `::-webkit-scrollbar { display: none; }
-//            //    body { overflow: hidden !important; }
-//            //`; document.head.appendChild(style); ");
-//        };
+        GitHubWebView2.Loaded += GitHubPreview_Loaded;
+
+        //        GitHubPreview.CoreWebView2Initialized += (_, __) =>
+        //        {
+        //            // TODO: Is this call ever reached?
+        //            var wv = GitHubPreview.CoreWebView2;
+        //
+        //            // Disable context menus, dev tools, zoom, and status bar
+        //            wv.Settings.AreDefaultContextMenusEnabled = false;
+        //            wv.Settings.AreDevToolsEnabled = false;
+        //            wv.Settings.IsZoomControlEnabled = false;
+        //            wv.Settings.IsStatusBarEnabled = false;
+        //
+        //            // Disable built-in error page to prevent it from showing when loading fails
+        //            //wv.Settings.IsBuiltInErrorPageEnabled = false;
+        //
+        //            // Inject CSS to hide Scrollbars and prevent scrolling (optional, can be adjusted as needed)
+        //            //wv.AddScriptToExecuteOnDocumentCreatedAsync(@"
+        //            //const style = document.createElement('style');
+        //            //style.innerHTML = 
+        //            //    `::-webkit-scrollbar { display: none; }
+        //            //    body { overflow: hidden !important; }
+        //            //`; document.head.appendChild(style); ");
+        //        };
+    }
+
+    private async void GitHubPreview_Loaded(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await GitHubWebView2.EnsureCoreWebView2Async();
+            GitHubWebView2.Source = new Uri("https://github.com/vanara");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("WebView2 init failed: " + ex);
+        }
     }
 }
