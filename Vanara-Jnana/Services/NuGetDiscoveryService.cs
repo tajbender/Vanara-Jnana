0b6606d367d3a15b1781b4aea4654e6f1547a866
Thorsten Jung <tajbender@users.noreply.github.com>
2026-06-08 01:56:32 +0200

    Add `NuGetDiscoveryService.cs`

---
+++ Vanara-Jnana/Services/NuGetDiscoveryService.cs
@@ -0,0 +1,30 @@
+﻿using NuGet.Versioning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jnana.Services;
+
+public record PackageId(string Id);
+
+public record PackageVersionInfo(
+    string Id,
+    NuGetVersion Version,
+    Uri? IconUrl,
+    string? Description,
+    string? Summary
+);
+
+public record PackageContent(
+    string Id,
+    NuGetVersion Version,
+    IReadOnlyList<string> Assemblies,
+    string? Readme,
+    byte[]? IconBytes
+);
+
+internal class NuGetDiscoveryService
+{
+}
