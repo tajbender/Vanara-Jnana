492bd9de2e32ee2533ae7b8cc4b740b31668f154
Thorsten Jung <tajbender@users.noreply.github.com>
2026-06-10 12:13:11 +0200

    Add `ImageExtensions.cs` Helper

---
+++ Vanara-Jnana/Helpers/ImageExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jnana.Helpers;
+
+public class ImageExtensions
+{
+    public static BitmapImage? ToBitmapImage(byte[]? bytes)
+    {
+        if (bytes == null || bytes.Length == 0)
+            return null;
+
+        var bitmap = new BitmapImage();
+
+        using var ms = new MemoryStream(bytes);
+        var ras = ms.AsRandomAccessStream();
+
+        bitmap.SetSource(ras);
+        return bitmap;
+    }
+}
