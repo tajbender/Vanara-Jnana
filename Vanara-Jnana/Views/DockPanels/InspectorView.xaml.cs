34fb7080a665004013f8a56a13032b8d0e768e2f
Thorsten Jung <tajbender@users.noreply.github.com>
2026-07-10 09:21:02 +0200

    Add `Jnana.Views.DockPanels.InspectorView`

---
+++ Vanara-Jnana/Views/DockPanels/InspectorView.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Jnana.Views.DockPanels;
+
+/// <summary>
+/// A dockable Page that can be used to display information in a docked panel within the application.
+/// </summary>
+public sealed partial class InspectorView : Page
+{
+    public InspectorView()
+    {
+        InitializeComponent();
+    }
+}
