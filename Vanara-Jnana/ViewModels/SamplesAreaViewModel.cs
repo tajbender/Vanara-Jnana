9388f514fa60680374277759995760e689b03a7e
Thorsten Jung <tajbender@users.noreply.github.com>
2026-07-11 19:25:04 +0200

    Add sealed SampleInfo class to SamplesAreaViewModel
    Introduced the SampleInfo class with properties for Title, Description, Category, RunCommand (ICommand), and CodeLink, all with init-only setters. Added System.Windows.Input using directive to support ICommand.

--- Vanara-Jnana/ViewModels/SamplesAreaViewModel.cs
+++ Vanara-Jnana/ViewModels/SamplesAreaViewModel.cs
@@ -1,5 +1,15 @@
-﻿namespace Jnana.ViewModels;
+﻿using System.Windows.Input;
+
+namespace Jnana.ViewModels;
 
 public class SamplesAreaViewModel
 {
+    public sealed class SampleInfo
+    {
+        public string Title { get; init; }
+        public string Description { get; init; }
+        public string Category { get; init; }
+        public ICommand RunCommand { get; init; }
+        public string CodeLink { get; init; }
+    }
 }
