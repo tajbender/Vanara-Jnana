a18a2ab058348b494e3674cba6368e8c4a46e9e4
Thorsten Jung <tajbender@users.noreply.github.com>
2026-07-15 13:50:41 +0200

    Add SysInfoIdentityViewModel for system info display
    Added SysInfoIdentityViewModel to expose system info properties (OS version, machine name, workbench version, WinAppSdk version, Windows SDK version). Updated UtilitiesAreaViewModel to use this new class. Removed unused usings and adjusted namespace style.

--- Vanara-Jnana/ViewModels/UtilitiesAreaViewModel.cs
+++ Vanara-Jnana/ViewModels/UtilitiesAreaViewModel.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Jnana.ViewModels;
 
-namespace Jnana.ViewModels;
+public class SysInfoIdentityViewModel
+{
+    public string OSVersion => Environment.OSVersion.ToString();
+    public string MachineName => Environment.MachineName;
+    public string WorkbenchVersion => "5.0.5";
+
+    public string WinAppSdkVersion => typeof(Microsoft.UI.Xaml.Application).Assembly.GetName().Version.ToString();
+    public string WindowsSdkVersion => "10.0.22621"; // später dynamisch
+}
+
 
 internal class UtilitiesAreaViewModel
 {
+    private readonly SysInfoIdentityViewModel _sysInfoIdentityViewModel;
+
+    public UtilitiesAreaViewModel()
+    {
+        _sysInfoIdentityViewModel = new SysInfoIdentityViewModel();
+    }
 }
