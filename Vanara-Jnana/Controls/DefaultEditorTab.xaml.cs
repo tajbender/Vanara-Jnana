70d28f8806f6bfad4ea567a42482d1a917d752d7
Thorsten Jung <tajbender@users.noreply.github.com>
2026-07-18 21:47:43 +0200

    Refactor DefaultEditorTab and ViewModel structure
    - Moved DefaultEditorTabViewModel above DefaultEditorTab and made it internal
    - Changed Title default to "[DefaultEditorTab]"
    - Removed/commented out InitializeComponent() call
    - Updated ViewModel property to strongly-typed DefaultEditorTabViewModel and added ViewModelTyped
    - Set DataContext to ViewModel in constructor

--- Vanara-Jnana/Controls/DefaultEditorTab.xaml.cs
+++ Vanara-Jnana/Controls/DefaultEditorTab.xaml.cs
@@ -2,19 +2,22 @@ using Microsoft.UI.Xaml.Controls;
 
 namespace Jnana.Views.Tabs;
 
+internal class DefaultEditorTabViewModel
+{
+    public string Title { get; set; } = "[DefaultEditorTab]";
+}
+
+
+
 public sealed partial class DefaultEditorTab : Page
 {
     private object ViewModel { get; set; } = new DefaultEditorTabViewModel();
 
+    private DefaultEditorTabViewModel ViewModelTyped => (DefaultEditorTabViewModel)ViewModel;
+
     public DefaultEditorTab()
     {
-        InitializeComponent();
-        this.ViewModel = new DefaultEditorTabViewModel();
+        //InitializeComponent();
         this.DataContext = ViewModel;
     }
-}
-
-internal class DefaultEditorTabViewModel
-{
-    public string Title { get; set; } = "Default Editor Tab";
 }
