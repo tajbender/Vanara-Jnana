b95c3c3d9d5705dd49a2a65442bae6589209eb78
Thorsten Jung <tajbender@users.noreply.github.com>
2026-07-18 13:19:47 +0200

    Add QuickLaunchDockPanel UserControl and update project file
    Introduced QuickLaunchDockPanel UserControl in Views/DockPanels with code-behind. Updated Jnana.csproj to compile the new XAML file and removed it from the None item group.

---
+++ Vanara-Jnana/Views/DockPanels/QuickLaunchDockPanel.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+
+namespace Jnana.Views.DockPanels;
+
+public sealed partial class QuickLaunchDockPanel : UserControl
+{
+    public QuickLaunchDockPanel()
+    {
+        InitializeComponent();
+    }
+}
