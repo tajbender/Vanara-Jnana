b87ecc914aa002ac08d961ee4a7bddb65eae45b1
Thorsten Jung <tajbender@users.noreply.github.com>
2026-07-19 13:08:20 +0200

    Refactor ThemeService; clean up image assets
    Refactored ThemeService to public, added theme management with current theme storage, ThemeChanged event, and set/apply methods (ApplyTheme commented out). Removed unused image assets from project for both Dark and Light themes.

--- Vanara-Jnana/Services/ThemeService.cs
+++ Vanara-Jnana/Services/ThemeService.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using Jnana;
+using Microsoft.UI.Xaml;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Jnana.App;
 
 namespace Vanara_Jnana.exe.Services;
 
-internal class ThemeService
+public class ThemeService
 {
+    public AppTheme CurrentTheme { get; private set; } = AppTheme.System;
+
+    public event EventHandler<AppTheme>? ThemeChanged;
+
+    public void SetTheme(AppTheme theme)
+    {
+        CurrentTheme = theme;
+        ApplyTheme(theme);
+        ThemeChanged?.Invoke(this, theme);
+    }
+
+    private void ApplyTheme(AppTheme theme)
+    {
+//        var root = (FrameworkElement)App.MainWindow.Content;
+//
+//        root.RequestedTheme = theme switch
+//        {
+//            AppTheme.Light => ElementTheme.Light,
+//            AppTheme.Dark => ElementTheme.Dark,
+//            _ => ElementTheme.Default
+//        };
+    }
 }
