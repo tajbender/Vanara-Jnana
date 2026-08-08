eae2c251e29fa02c234f801af0073c5af480bfc6
Thorsten Jung <tajbender@users.noreply.github.com>
2026-08-08 14:28:16 +0200

    Refactor navigation, add i18n class, update project files
    - Added static i18n class with JnanaFullName property in Assets/i18n.cs.
    - Refactored NavigationService for multi-host support, events, improved logic, and error handling; added back navigation.
    - Renamed and reorganized grid/navigation hosts in MainWindow.xaml.
    - Updated MainWindow.xaml.cs to use three NavigationService instances for new host structure.
    - Removed Assets/i18n.cs from Vanara Jnana.csproj content and adjusted project file.

--- Core/Navigation/NavigationService.cs
+++ Core/Navigation/NavigationService.cs
@@ -5,28 +5,77 @@ namespace Jnana.Core.Navigation;
 
 public class NavigationService : INavigationService
 {
-    //private readonly Frame _frame;
+    private readonly Frame _frame;
+    private readonly NavigationHost _host;
 
-    public Type? CurrentPage { get; private set; }
+    public Type? CurrentPage { get; private set; } = null;
+    public NavigationHost SecondaryRightSidebarHost { get; }
 
-    /*  // private readonly Frame _frame;
-        public NavigationService(Frame frame) => _frame = frame; */
+    public event EventHandler Navigated;
+
+    public NavigationService(NavigationHost host, Frame frame)
+    {
+        _host = host;
+        _frame = frame;
+
+        // pageInstance
+        //_PrimaryNavigation = new NavigationService(MainGridHost);
+
+    }
+
+    public NavigationService(NavigationHost secondaryRightSidebarHost)
+    {
+        this.SecondaryRightSidebarHost = secondaryRightSidebarHost;
+    }
 
     public void Navigate(Type pageType)
     {
-        CurrentPage = pageType;
-        // TODO: Implement navigation logic:
-        //       public void Navigate(Type pageType) => _frame.Navigate(pageType);
+        try
+        {
+            var page = Activator.CreateInstance(pageType);
+            CurrentPage = pageType;
+
+            // TODO: Implement navigation logic:
+            //ShowPage(page);
+
+            Navigated?.Invoke(this, EventArgs.Empty);
+        }
+        catch
+        {
+            CurrentPage = null;
+        }
+    }
+
+    private void ShowPage(Frame page)
+    {
+        if (CurrentPage != null)
+        {
+            try
+            {
+                _frame.Navigate(page.GetType());
+            }
+            catch
+            {
+                // Handle navigation failure
+            }
+        }
     }
 
     public void Navigate<TPage>() where TPage : class
     {
         Navigate(typeof(TPage));
+
+        Navigated?.Invoke(this, EventArgs.Empty);
     }
 
+    // TODO: Implement back navigation
     public void GoBack()
     {
-        // TODO: Implement back navigation
-        //       public void GoBack() { if (_frame.CanGoBack) _frame.GoBack(); }     
+        if (CurrentPage != null)
+        {
+            if (_frame.CanGoBack)
+                _frame.GoBack();
+            Navigated?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,16 +6,21 @@ namespace Jnana;
 
 public sealed partial class MainWindow : Window
 {
-    private readonly NavigationService _navigation;
+    private readonly NavigationService _PrimaryNavigation;
+    private readonly NavigationService _SecondaryNavigation;
+    private readonly NavigationService _SidebarNavigation;
 
     public MainWindow()
     {
         InitializeComponent();
 
-        _navigation = new NavigationService();
-
         var workbench = new WorkbenchPage();
-        _navigation.Navigate(typeof(WorkbenchPage));
-        //MainGridHost.ShowPage(workbench);
+        _PrimaryNavigation = new NavigationService(MainWindowHost);
+        _SecondaryNavigation = new NavigationService(SecondaryWindowHost);
+        _SidebarNavigation = new NavigationService(SecondaryRightSidebarHost);
+
+        //        _PrimaryNavigation.Navigate(typeof(WorkbenchPage));
+        //        //_PrimaryNavigation.ShowPage(workbench);
+        //        //MainGridHost.ShowPage(workbench);
     }
 }
